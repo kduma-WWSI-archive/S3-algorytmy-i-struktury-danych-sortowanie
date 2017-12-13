242d7fe7a4a4f798fe7cf862b7263743ccc7ffc6
Krystian Duma <krystian@muzik.pl>
2017-12-13 20:53:57 +0100

    Zmiana funkcji zamien_wartosci_zmiennych na std::swap

--- src/funkcje_wspolne.cpp
+++ src/funkcje_wspolne.cpp
@@ -4,12 +4,6 @@
 
 #include "funkcje_wspolne.h"
 
-void zamien_wartosci_zmiennych(int &wartosc_a, int &wartosc_b) {
-	int tymczasowa = wartosc_a;
-	wartosc_a = wartosc_b;
-	wartosc_b = tymczasowa;
-}
-
 std::string pad_right(const std::string &str, size_t s) {
 	if ( str.size() < s )
 		return str + std::string(s-str.size(), ' ');
