0ca4c445a2459d9f6011b15b91ee75c22a490499
Krystian Duma <krystian@muzik.pl>
2017-12-13 01:35:46 +0100

    Przygotowanie funkci main

--- main.cpp
+++ main.cpp
@@ -5,42 +5,28 @@
 #include "src/s_szybkie.h"
 #include "src/s_scalanie.h"
 #include "src/s_babelkowe.h"
+#include "src/generator_danych_testowych.h"
+#include "src/funkcje_we_wy.h"
+#include "test/testy.h"
+
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	uruchom_testy();
+
+	int n = 10;
+
+	w_int losowe, uporzadkowane, odwrotne, wynik;
+	generuj_dane_testowe(n, losowe, uporzadkowane, odwrotne);
+
+	wypisz_wyniki("losowe", losowe);
+	wypisz_wyniki("uporzadkowane", uporzadkowane);
+	wypisz_wyniki("odwrotne", odwrotne);
+
 	return 0;
 }
 
 
-// definicje tablic i stałych globalnych :
-int const N=1000001;
-int uporz[N];
-int los[N];
-int odwr[N];
-int a[N]; //tablica, na której działają algorytmy
 
-// definicje funkcji sortujących:
-
-
-
-
-
-
-//// użycie generatora liczb pseudolosowych
-//
-//funckja main():
-//definicja i zainicjowanie generatora liczb pseudolosowych
-//
-//int n;  // aktualna liczba elementów ciągu
-//cin>>n;
-//
-//przygotowanie tablic z danymi (los[], uporz[] i odwr[]):
-//wygenerowanie ciągu losowych o długości n i zapisanie go w tablicy los[];
-//skopiowanie los[] do a[];
-//uporządkowanie tablicy a[] algorytmem sortowania;
-//skopiowanie a[] do uporz[];
-//skopiowanie w odwrotnym porządku uporz[] do odwr[];
-//
 //obliczenia:
 //		skopiowanie uporz[] do a[];
 //sortowanie za pomocą badanego algorytmu;
