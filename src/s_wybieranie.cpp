8678bb90641bf0581f6790493f2d7847d0539429
Grzesiek9409 <34491750+Grzesiek9409@users.noreply.github.com>
2017-12-13 19:09:14 +0100

    Update s_wybieranie.cpp

--- src/s_wybieranie.cpp
+++ src/s_wybieranie.cpp
@@ -5,5 +5,21 @@
 #include "s_wybieranie.h"
 
 void wybieranie(const w_int &wejscie, w_int &wyjscie, int liczba_elementow) {
+  int a;
+  wejscie=wyjscie
+  for (int i=0;i<liczba_elementow;i++){
+  a=i;
+    for(int j=i+1;j<(liczba_elementow);j++){
+    
+      if(wyjscie[j]<wyjscie[a]){
+      a=j;
+        swap(wyjscie[a]>wyjscie[i]);
+      }
+    
+    }
+  
+}
+  
+  
 
 }
