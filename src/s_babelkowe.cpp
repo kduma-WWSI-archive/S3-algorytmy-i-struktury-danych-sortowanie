ff4e98640addcb93747c9951288598258cb9a742
Grzesiek9409 <34491750+Grzesiek9409@users.noreply.github.com>
2017-12-13 18:41:21 +0100

    Update s_babelkowe.cpp

--- src/s_babelkowe.cpp
+++ src/s_babelkowe.cpp
@@ -1,9 +1,22 @@
 //
-// Created by Krystian Duma on 12/12/2017.
+// Created by Krystian Duma AND ... on 12/12/2017.
 //
 
 #include "s_babelkowe.h"
 
 void babelkowe(const w_int &wejscie, w_int &wyjscie, int liczba_elementow) {
+  wejscie=wyjscie
+  for (int i=0;i<liczba_elementow;i++){
+  
+    for(int j=0;j<(liczba_elementow-1);i++){
+    
+      if(wyjscie[j]>wyjscie[j-1]){
+      
+        swap(wyjscie[j]>wyjscie[j+1]);
+      }
+    
+    }
+  
+  }
 
 }
