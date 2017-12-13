40c6c9600abdc69b5c2f1f586a2806afefb88ea4
Grzesiek9409 <34491750+Grzesiek9409@users.noreply.github.com>
2017-12-13 20:15:11 +0100

    Update s_babelkowe.cpp

--- src/s_babelkowe.cpp
+++ src/s_babelkowe.cpp
@@ -5,7 +5,7 @@
 #include "s_babelkowe.h"
 
 void babelkowe(const w_int &wejscie, w_int &wyjscie, int liczba_elementow) {
-  wejscie=wyjscie
+  wyjscie=wejscie;
   for (int i=0;i<liczba_elementow;i++){
   
     for(int j=0;j<(liczba_elementow-1);j++){
