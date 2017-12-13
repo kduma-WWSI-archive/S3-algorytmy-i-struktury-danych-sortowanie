2cc84b2228bb174959b391a7d8eefd6defebe766
Grzesiek9409 <34491750+Grzesiek9409@users.noreply.github.com>
2017-12-13 20:14:36 +0100

    Update s_wybieranie.cpp

--- src/s_wybieranie.cpp
+++ src/s_wybieranie.cpp
@@ -6,7 +6,7 @@
 
 void wybieranie(const w_int &wejscie, w_int &wyjscie, int liczba_elementow) {
   int a;
-  wejscie=wyjscie
+  wyjscie=wejscie;
   for (int i=0;i<liczba_elementow;i++){
   a=i;
     for(int j=i+1;j<(liczba_elementow);j++){
