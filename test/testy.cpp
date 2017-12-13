75eb30d75989c4a250016ead0b72fe6f5edd02b8
Krystian Duma <krystian@muzik.pl>
2017-12-13 01:40:28 +0100

    Czyszczenie

--- test/testy.cpp
+++ test/testy.cpp
@@ -12,8 +12,6 @@
 #include "../src/funkcje_we_wy.h"
 
 
-
-
 void uruchom_testy() {
 	w_int dane_wejsciowe{1,6,4,7,2,5,9,0,3,8};
 	w_int oczekiwane_dane_wyjsciowe{0,1,2,3,4,5,6,7,8,9};
@@ -31,7 +29,6 @@ void testuj_babelkowe(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_
 	babelkowe(dane_wejsciowe, dane_wyjsciowe, static_cast<int>(dane_wejsciowe.size()));
 
 	porownaj_wyniki("Sortowanie Bąbelkowe", oczekiwane_dane_wyjsciowe, dane_wyjsciowe);
-
 }
 
 void testuj_scalanie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_wyjsciowe) {
@@ -41,7 +38,6 @@ void testuj_scalanie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_w
 	sps(dane_wejsciowe, dane_wyjsciowe, liczba_elementow, 0, liczba_elementow-1);
 
 	porownaj_wyniki("Sortowanie przez Scalanie", oczekiwane_dane_wyjsciowe, dane_wyjsciowe);
-
 }
 
 void testuj_szybkie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_wyjsciowe) {
@@ -51,7 +47,6 @@ void testuj_szybkie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_wy
 	szybkie(dane_wejsciowe, dane_wyjsciowe, liczba_elementow, 0, liczba_elementow-1);
 
 	porownaj_wyniki("Sortowanie Szybkie", oczekiwane_dane_wyjsciowe, dane_wyjsciowe);
-
 }
 
 void testuj_wstawianie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_wyjsciowe) {
@@ -60,7 +55,6 @@ void testuj_wstawianie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane
 	wstawianie(dane_wejsciowe, dane_wyjsciowe, static_cast<int>(dane_wejsciowe.size()));
 
 	porownaj_wyniki("Sortowanie przez Wstawianie", oczekiwane_dane_wyjsciowe, dane_wyjsciowe);
-
 }
 
 void testuj_wybieranie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane_wyjsciowe) {
@@ -69,7 +63,6 @@ void testuj_wybieranie(const w_int &dane_wejsciowe, const w_int &oczekiwane_dane
 	wybieranie(dane_wejsciowe, dane_wyjsciowe, static_cast<int>(dane_wejsciowe.size()));
 
 	porownaj_wyniki("Sortowanie przez Wybieranie", oczekiwane_dane_wyjsciowe, dane_wyjsciowe);
-
 }
 
 void porownaj_wyniki(const std::string &algorytm, const w_int &oczekiwane_dane_wyjsciowe, const w_int &dane_wyjsciowe) {
