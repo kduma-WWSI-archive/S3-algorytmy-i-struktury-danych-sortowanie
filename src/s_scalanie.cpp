fd7c5565db90f92cf2ededeed934787169ee6e21
Krystian Duma <krystian@muzik.pl>
2017-12-14 00:25:00 +0100

    Szybszy algorytm sortowania przez scalanie

--- src/s_scalanie.cpp
+++ src/s_scalanie.cpp
@@ -4,50 +4,44 @@
 
 #include "s_scalanie.h"
 
-//scalenie posortowanych podtablic
-void scal(w_int &tab, int lewy, int srodek, int prawy)
+void mergesort (w_int &a, int num)
 {
-	w_int pom(tab.size());
+	w_int b(a.size());
+	int rght, wid, rend;
+	int i,j,m,t;
 
-	int i = lewy, j = srodek + 1;
-
-	//kopiujemy lewą i prawą część tablicy do tablicy pomocniczej
-	for(int i = lewy;i<=prawy; i++)
-		pom[i] = tab[i];
-
-	//scalenie dwóch podtablic pomocniczych i zapisanie ich
-	//we własciwej tablicy
-	for(int k=lewy;k<=prawy;k++)
-		if(i<=srodek)
-			if(j <= prawy)
-				if(pom[j]<pom[i])
-					tab[k] = pom[j++];
-				else
-					tab[k] = pom[i++];
-			else
-				tab[k] = pom[i++];
-		else
-			tab[k] = pom[j++];
-}
-
-void sortowanie_przez_scalanie(w_int &tab, int lewy, int prawy)
-{
-	//gdy mamy jeden element, to jest on już posortowany
-	if(prawy<=lewy) return;
-
-	//znajdujemy srodek podtablicy
-	int srodek = (prawy+lewy)/2;
-
-	//dzielimy tablice na częsć lewą i prawa
-	sortowanie_przez_scalanie(tab, lewy, srodek);
-	sortowanie_przez_scalanie(tab, srodek+1, prawy);
-
-	//scalamy dwie już posortowane tablice
-	scal(tab, lewy, srodek, prawy);
+	for (int k=1; k < num; k *= 2 ) {
+		for (int left=0; left+k < num; left += k*2 ) {
+			rght = left + k;
+			rend = rght + k;
+			if (rend > num) rend = num;
+			m = left; i = left; j = rght;
+			while (i < rght && j < rend) {
+				if (a[i] <= a[j]) {
+					b[m] = a[i]; i++;
+				} else {
+					b[m] = a[j]; j++;
+				}
+				m++;
+			}
+			while (i < rght) {
+				b[m]=a[i];
+				i++; m++;
+			}
+			while (j < rend) {
+				b[m]=a[j];
+				j++; m++;
+			}
+			for (m=left; m < rend; m++) {
+				a[m] = b[m];
+			}
+		}
+	}
 }
 
 void sps(const w_int &wejscie, w_int &wyjscie, int liczba_elementow) {
 	wyjscie = wejscie;
 
-	sortowanie_przez_scalanie(wyjscie, 0, liczba_elementow-1);
+//	sortowanie_przez_scalanie(wyjscie, 0, liczba_elementow-1);
+	mergesort(wyjscie, liczba_elementow);
 }
