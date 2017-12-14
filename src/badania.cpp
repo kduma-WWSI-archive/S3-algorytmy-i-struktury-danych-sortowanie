9a9a943eb1abb27d3584af892a8d3a32b394e751
Krystian Duma <krystian@muzik.pl>
2017-12-14 05:05:20 +0100

    Optymalizacje

--- src/badania.cpp
+++ src/badania.cpp
@@ -28,19 +28,22 @@ jednostka_badan badaj_algorytmy(int liczba_elementow, bool duza_zmiennosc) {
 	badanie.babelkowe = czasy_dla_algorytmu("babelkowe", babelkowe, liczba_elementow, losowe, uporzadkowane, odwrotne);
 	badanie.scalanie = czasy_dla_algorytmu("scalanie", sps, liczba_elementow, losowe, uporzadkowane, odwrotne);
 
-	if(liczba_elementow < 100000){
-		badanie.szybkie_lewy = czasy_dla_algorytmu("szybkie_lewy", szybkie_l, liczba_elementow, losowe, uporzadkowane, odwrotne);
-		badanie.szybkie_srodek = czasy_dla_algorytmu("szybkie_srodek", szybkie_s, liczba_elementow, losowe, uporzadkowane, odwrotne);
+	if(liczba_elementow >= 100000 && duza_zmiennosc){
+		badanie.szybkie_lewy = czasy_dla_algorytmu("szybkie_lewy", szybkie_l, liczba_elementow, losowe, losowe, losowe, uporzadkowane);
+		badanie.szybkie_lewy.odwrotne = -1;
+		badanie.szybkie_lewy.uporzadkowane = -1;
 	} else {
-		badanie.szybkie_lewy = {-1, -1, -1};
-		badanie.szybkie_srodek = {-1, -1, -1};
+		badanie.szybkie_lewy = czasy_dla_algorytmu("szybkie_lewy", szybkie_l, liczba_elementow, losowe, uporzadkowane, odwrotne, uporzadkowane);
 	}
+
+	badanie.szybkie_srodek = czasy_dla_algorytmu("szybkie_srodek", szybkie_s, liczba_elementow, losowe, uporzadkowane, odwrotne, uporzadkowane);
 
 	return badanie;
 }
 
-czasy_wynikowe czasy_dla_algorytmu(const std::string &algorytm, void (*funkcja_sortujaca)(const w_int &, w_int &, int), int liczba_elementow,
-								   const w_int &losowe, const w_int &uporzadkowane, const w_int &odwrotne) {
+czasy_wynikowe czasy_dla_algorytmu(const std::string &algorytm, void (*funkcja_sortujaca)(const w_int &, w_int &, int),
+								   int liczba_elementow, const w_int &losowe, const w_int &uporzadkowane,
+								   const w_int &odwrotne, const w_int &do_porownania) {
 	czasy_wynikowe czasy{};
 	StopWatch timer;
 	w_int wynik;
@@ -49,19 +52,19 @@ czasy_wynikowe czasy_dla_algorytmu(const std::string &algorytm, void (*funkcja_s
 	timer.start();
 	funkcja_sortujaca(losowe, wynik, liczba_elementow);
 	czasy.losowe = timer.stop();
-	naglowek_porownania(pad_right(algorytm+";", 15)+" dane losowe;        n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.losowe)+" sekund", wynik, uporzadkowane);
+	naglowek_porownania(pad_right(algorytm+";", 15)+" dane losowe;        n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.losowe)+" sekund", wynik, do_porownania);
 
 	wynik.resize(0);
 	timer.start();
 	funkcja_sortujaca(uporzadkowane, wynik, liczba_elementow);
 	czasy.uporzadkowane = timer.stop();
-	naglowek_porownania(pad_right(algorytm+";", 15)+" dane uporzadkowane; n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.uporzadkowane)+" sekund", wynik, uporzadkowane);
+	naglowek_porownania(pad_right(algorytm+";", 15)+" dane uporzadkowane; n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.uporzadkowane)+" sekund", wynik, do_porownania);
 
 	wynik.resize(0);
 	timer.start();
 	funkcja_sortujaca(odwrotne, wynik, liczba_elementow);
 	czasy.odwrotne = timer.stop();
-	naglowek_porownania(pad_right(algorytm+";", 15)+" dane odwrotne;      n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.odwrotne)+" sekund", wynik, uporzadkowane);
+	naglowek_porownania(pad_right(algorytm+";", 15)+" dane odwrotne;      n = "+pad_right(std::to_string(liczba_elementow)+";", 10)+std::to_string(czasy.odwrotne)+" sekund", wynik, do_porownania);
 
 	return czasy;
 }
--- src/badania.h
+++ src/badania.h
@@ -9,7 +9,8 @@
 
 	jednostka_badan badaj_algorytmy(int liczba_elementow, bool duza_zmiennosc);
 	czasy_wynikowe
-	czasy_dla_algorytmu(const std::string &algorytm, void (*funkcja_sortujaca)(const w_int &, w_int &, int), int liczba_elementow,
-							const w_int &losowe, const w_int &uporzadkowane, const w_int &odwrotne);
+	czasy_dla_algorytmu(const std::string &algorytm, void (*funkcja_sortujaca)(const w_int &, w_int &, int),
+							int liczba_elementow, const w_int &losowe, const w_int &uporzadkowane,
+							const w_int &odwrotne, const w_int &do_porownania);
 
 #endif //SORTOWANIE_BADANIA_H
